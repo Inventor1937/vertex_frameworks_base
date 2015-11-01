571e7736af717e4b53ebcb3a361dba7eb6e98545
Carlo Savignano <carlosavignano@aospa.co>
2015-11-01 22:31:30 +0800

    input: support dynamic navigation bar toggling
    * Add system setting to dynamically switch navigation bar on/off.
    * Addresses NOUGAT-x
    
    Change-Id: I55da79fe8da83cf839cc91c1257ea91d2d632a40
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10528,6 +10528,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean needsNavigationBar() {
+        return mPolicy.needsNavigationBar();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
