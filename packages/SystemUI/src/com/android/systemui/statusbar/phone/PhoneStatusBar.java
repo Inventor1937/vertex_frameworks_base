bf002be89472d5f97ccaaba84ac235db01789bfa
Carlo Savignano <carlosavignano@aospa.co>
2017-01-05 13:11:56 +0100

    SystemUI: support swapping navigation bar keys order
    * Use new API from SystemUI tuner that allows inflate
      custom layout on the fly.
    
    TODO> Allow SystemUI Tuner to override this when enabled and changes
          are made after enabling it. When disabling the tuner then use
          default arrangement and apply swap only.
    
    Change-Id: Ib1993140eb1bf7dc0017d75015516db95ce70d9a
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -588,6 +588,22 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     };
 
+    final private ContentObserver mSwapNavKeyObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange) {
+            boolean wasUsing = mUseSwapKey;
+            mUseSwapKey = Settings.System.getIntForUser(
+                    mContext.getContentResolver(), Settings.System.SWAP_NAVIGATION_KEYS, 0,
+                    UserHandle.USER_CURRENT) != 0;
+            Log.d(TAG, "navbar is " + (mUseSwapKey ? "swapped" : "regular"));
+            if (wasUsing != mUseSwapKey) {
+                if (mNavigationBarView != null) {
+                    prepareNavigationBarView();
+                }
+            }
+        }
+    };
+
     private int mInteractingWindows;
     private boolean mAutohideSuspended;
     private int mStatusBarMode;
@@ -866,6 +882,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mScreenPinningRequest = new ScreenPinningRequest(mContext);
         mFalsingManager = FalsingManager.getInstance(mContext);
+
+        mSwapNavKeyObserver.onChange(true); // set up
     }
 
     protected void createIconController() {
@@ -1182,6 +1200,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // listen for NAVIGATION_BAR_ENABLED setting (per-user)
         resetNavBarObserver();
+
+        // listen for SWAP_NAVIGATION_KEYS setting (per-user)
+        resetSwapNavKeyObserver();
 
         return mStatusBarView;
     }
@@ -1604,6 +1625,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private void prepareNavigationBarView() {
+        mNavigationBarView.setSwapKeys(mUseSwapKey);
         mNavigationBarView.reorient();
 
         ButtonDispatcher recentsButton = mNavigationBarView.getRecentsButton();
@@ -3979,6 +4001,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         super.userSwitched(newUserId);
         if (MULTIUSER_DEBUG) mNotificationPanelDebugText.setText("USER " + newUserId);
         resetNavBarObserver();
+        resetSwapNavKeyObserver();
         animateCollapsePanels();
         updatePublicMode();
         updateNotifications();
@@ -4013,6 +4036,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.NAVIGATION_BAR_ENABLED), true,
                 mNavBarObserver, mCurrentUserId);
+    }
+
+    private void resetSwapNavKeyObserver() {
+        mContext.getContentResolver().unregisterContentObserver(mSwapNavKeyObserver);
+        mSwapNavKeyObserver.onChange(false);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.SWAP_NAVIGATION_KEYS), true,
+                mSwapNavKeyObserver, mCurrentUserId);
     }
 
     /**
