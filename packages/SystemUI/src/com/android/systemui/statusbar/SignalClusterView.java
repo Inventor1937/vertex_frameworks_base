27923b34d9ff73698293ddde5f9cbcd5a6e7ec5e
xyyx <xyyx@mail.ru>
2017-02-12 12:37:45 +0800

    Add VOLTE icon
    Extracted from CAF and adapted to AOSP by @xyyx
    Added switch to statusbar icon blacklist by @xyyx
    modified back to caf base by @xlxfoxxlx
    
    Original commits:
    https://github.com/codeaurora-unofficial/platform-frameworks-base/commit/8c968e8d64abca36987d90d95e6dd1cc951c0e0a
    https://github.com/AOSPA/android_frameworks_base/commit/4dabbbe7df5f51f517c13da98809c22b34175964
    
    Change-Id: Ie85b73fa9f5818aeb237742340fd45e6e7affefd

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -64,6 +64,7 @@ public class SignalClusterView
     private static final String SLOT_WIFI = "wifi";
     private static final String SLOT_ETHERNET = "ethernet";
     private static final String SLOT_VPN = "vpn";
+    private static final String SLOT_VOLTE = "volte";
 
     NetworkControllerImpl mNC;
     SecurityController mSC;
@@ -112,8 +113,10 @@ public class SignalClusterView
     private boolean mBlockMobile;
     private boolean mBlockWifi;
     private boolean mBlockEthernet;
+
     private TelephonyManager mTelephonyManager;
     private boolean mBlockVpn;
+    private boolean mBlockVolte;
 
     public SignalClusterView(Context context) {
         this(context, null);
@@ -151,14 +154,16 @@ public class SignalClusterView
         boolean blockWifi = blockList.contains(SLOT_WIFI);
         boolean blockEthernet = blockList.contains(SLOT_ETHERNET);
         boolean blockVpn = blockList.contains(SLOT_VPN);
+        boolean blockVolte = blockList.contains(SLOT_VOLTE);
 
         if (blockAirplane != mBlockAirplane || blockMobile != mBlockMobile
-                || blockEthernet != mBlockEthernet || blockWifi != mBlockWifi || blockVpn != mBlockVpn) {
+                || blockEthernet != mBlockEthernet || blockWifi != mBlockWifi || blockVpn != mBlockVpn || blockVolte != mBlockVolte) {
             mBlockAirplane = blockAirplane;
             mBlockMobile = blockMobile;
             mBlockEthernet = blockEthernet;
             mBlockWifi = blockWifi;
             mBlockVpn = blockVpn;
+            mBlockVolte = blockVolte;
             // Re-register to get new callbacks.
             mNC.removeSignalCallback(this);
             mNC.addSignalCallback(this);
@@ -295,7 +300,7 @@ public class SignalClusterView
     public void setMobileDataIndicators(IconState statusIcon, IconState qsIcon, int statusType,
             int qsType, boolean activityIn, boolean activityOut, int dataActivityId,
             int mobileActivityId, int stackedDataId, int stackedVoiceId,
-            String typeContentDescription, String description, boolean isWide, int subId) {
+            String typeContentDescription, String description, boolean isWide, int subId, boolean isMobileIms) {
         PhoneState state = getState(subId);
         if (state == null) {
             return;
@@ -310,6 +315,7 @@ public class SignalClusterView
         state.mMobileActivityId = mobileActivityId;
         state.mStackedDataId = stackedDataId;
         state.mStackedVoiceId = stackedVoiceId;
+        mMobileIms = isMobileIms;
 
         apply();
     }
@@ -330,7 +336,7 @@ public class SignalClusterView
         mMobileIms = isMobileIms;
         this.setMobileDataIndicators(statusIcon, qsIcon, statusType, qsType, activityIn,
                 activityOut, dataActivityId, mobileActivityId, stackedDataId,
-                stackedVoiceId, typeContentDescription, description, isWide, subId);
+                stackedVoiceId, typeContentDescription, description, isWide, subId, isMobileIms);
     }
 
     @Override
@@ -620,7 +626,7 @@ public class SignalClusterView
             mImsOverWifiImageView.setVisibility(View.GONE);
         }
 
-        if (mMobileIms){
+        if (mMobileIms && !mBlockVolte){
             mMobileImsImageView.setVisibility(View.VISIBLE);
         } else {
             mMobileImsImageView.setVisibility(View.GONE);
@@ -676,6 +682,7 @@ public class SignalClusterView
 
     private void applyIconTint() {
         setTint(mVpn, StatusBarIconController.getTint(mTintArea, mVpn, mIconTint));
+        setTint(mMobileImsImageView, StatusBarIconController.getTint(mTintArea, mMobileImsImageView, mIconTint));
         setTint(mAirplane, StatusBarIconController.getTint(mTintArea, mAirplane, mIconTint));
         setTint(mImsOverWifiImageView, StatusBarIconController.getTint(
             mTintArea, mImsOverWifiImageView, mIconTint));
