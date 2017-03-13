837cee56457047f77cb9bbfbaf2301968d2b5855
joshuous <joshuous@gmail.com>
2017-03-13 06:36:34 +0000

    base: Switch Reboot icon to ic_restart vector

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -389,7 +389,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final class RebootAction extends SinglePressAction implements LongPressAction {
         private RebootAction() {
-            super(com.android.internal.R.drawable.ic_lock_reboot,
+            super(com.android.internal.R.drawable.ic_restart,
                 R.string.global_action_reboot);
         }
 
