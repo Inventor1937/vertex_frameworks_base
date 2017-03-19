1a40c09b07e816106f9a5ffcf9c776d5c6f2f731
Sultanxda <sultanxda@gmail.com>
2017-03-19 12:54:46 +0000

    Fingerprint: Speed up wake-and-unlock scenario
    * Adapted for VertexOS on Nougat
    * Directly dismiss keyguard instead of using a fade-out animation
    * Skip virtual navigation button animation on wake
    
    Change-Id: Ic0bc19164eeca7254d72a09314391ecd3c750e50

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2374,9 +2374,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 if (DEBUG_MEDIA) {
                     Log.v(TAG, "DEBUG_MEDIA: Fading out album artwork");
                 }
-                if (mFingerprintUnlockController.getMode()
-                        == FingerprintUnlockController.MODE_WAKE_AND_UNLOCK_PULSING
-                        || hideBecauseOccluded) {
+                int fpMode = mFingerprintUnlockController.getMode();
+                if (fpMode == FingerprintUnlockController.MODE_WAKE_AND_UNLOCK_PULSING
+                        || hideBecauseOccluded
+                        || fpMode == FingerprintUnlockController.MODE_WAKE_AND_UNLOCK) {
 
                     // We are unlocking directly - no animation!
                     mBackdrop.setVisibility(View.GONE);
